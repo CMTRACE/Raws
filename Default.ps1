d527065e560202c4393d180e4384f957b9093d33
CMTRACE <51069646+CMTRACE@users.noreply.github.com>
2025-05-20 20:17:02 +1000

    Update Default.ps1

--- Default.ps1
+++ Default.ps1
@@ -4,6 +4,16 @@ Try {
     Write-Host "OSDCloud deployment completed."
 
     $UpdatePath = 'D:\osdcloud\windows11.0-kb5058411-x64_2025-05.msu'
+    $UpdateUrl = "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/770c53ae-5610-402f-b5e9-fe86142003cc/public/windows11.0-kb5058411-x64_fc93a482441b42bcdbb035f915d4be2047d63de5.msu"
+    $TempUpdatePath = "C:\Windows\Temp\windows11.0-kb5058411-x64_2025-05.msu"
+
+    if (-Not (Test-Path $UpdatePath)) {
+        Write-Host "Update file not found at $UpdatePath. Downloading from Microsoft Update Catalog..."
+        Invoke-WebRequest -Uri $UpdateUrl -OutFile $TempUpdatePath
+        $UpdatePath = $TempUpdatePath
+        Write-Host "Download complete: $UpdatePath"
+    }
+
     Write-Host "Adding Windows package: $UpdatePath"
     Add-WindowsPackage -PackagePath $UpdatePath -Path 'C:\'
     Write-Host "Windows package added."
