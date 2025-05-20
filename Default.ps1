ef928f4378e3d3c2085247862a1dbf041807804d
CMTRACE <51069646+CMTRACE@users.noreply.github.com>
2025-05-20 17:51:46 +1000

    Update Default.ps1

--- Default.ps1
+++ Default.ps1
@@ -1,5 +1,5 @@
 Start-OSDCloud -Firmware -ZTI -OSName 'Windows 11 24H2 x64' -OSEdition Enterprise -OSLanguage en-us -OSActivation Volume
-Add-WindowsPackage -PackagePath 'D:\CU\windows11.0-kb5058411-x64_2025-05.msu' -Path 'c:\'
+Add-WindowsPackage -PackagePath 'D:\CU\windows11.0-kb5058411-x64_2025-05.msu' -Path 'C:\'
 $process = Start-Process -FilePath "C:\Windows\System32\bcdboot.exe" -ArgumentList "C:\Windows", "/v", "/c" -Wait -NoNewWindow -PassThru
 $process = Start-Process -FilePath "C:\Windows\System32\DISM.exe" -ArgumentList "/Image:C:\", "/Cleanup-Image", "/StartComponentCleanup", "/ResetBase", "/ScratchDir:C:\Windows\Temp" -Wait -NoNewWindow -PassThru
 Restart-Computer
