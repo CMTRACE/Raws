b4c79c91c8abd21a1a58c4a51b125c1a28f2b0f8
CMTRACE <51069646+CMTRACE@users.noreply.github.com>
2025-05-20 17:28:47 +1000

    Update Default.ps1

--- Default.ps1
+++ Default.ps1
@@ -8,7 +8,7 @@ catch {
 }
 
 try {
-    Add-WindowsPackage -PackagePath 'D:\CU\windows11.0-kb5058411-x64_2025-05.msu' -Path 'c:\'
+    Add-WindowsPackage -PackagePath 'D:\osdcloud\windows11.0-kb5058411-x64_2025-05.msu' -Path 'c:\'
     Write-Host "Add-WindowsPackage completed successfully." -ForegroundColor Green
 }
 catch {
@@ -27,6 +27,17 @@ if ($process.ExitCode -eq 0) {
 catch {
     Write-Host "Error running bcdboot: $_" -ForegroundColor Red
     
+}
+
+try {
+    $process = Start-Process -FilePath "C:\Windows\System32\DISM.exe" -ArgumentList "/Image:C:\", "/Cleanup-Image", "/StartComponentCleanup", "/ResetBase" -Wait -NoNewWindow -PassThru
+    if ($process.ExitCode -eq 0) {
+    Write-Host "Image cleanup executed successfully." -ForegroundColor Green
+} else {
+    Write-Host "Image cleanup process failed with $($process.ExitCode)." -ForegroundColor Red}
+}
+catch {
+    Write-Host "Error running DISM: $_" -ForegroundColor Red
 }
 
 try {
