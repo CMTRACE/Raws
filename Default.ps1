a35e6876ddcffcb98213d76af86e2a334a9b537e
CMTRACE <51069646+CMTRACE@users.noreply.github.com>
2026-02-02 14:33:29 +1000

    Refactor Start-OSDCloud parameters into variables

--- Default.ps1
+++ Default.ps1
@@ -1,1 +1,21 @@
-Start-OSDCloud -Firmware -ZTI -OSName 'Windows 11 25H2 x64' -OSEdition Enterprise -OSLanguage en-us -OSActivation Volume -Restart
+$OSName = 'Windows 11 25H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'en-us'
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -Restart
